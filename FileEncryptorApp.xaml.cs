556d9ce0a286861de6eb96041b20976a525af7b6
ipzk-spa <ipzk241_spa@student.ztu.edu.ua>
2025-02-08 19:44:31 +0200

    Solved problem Long Method in FileEncryptorApp.xaml.cs file

--- FileEncryptorApp.xaml.cs
+++ FileEncryptorApp.xaml.cs
@@ -25,10 +25,7 @@ namespace FileEncryptorApp
 
         private void InitializeBackgroundWorker()
         {
-            worker = new BackgroundWorker
-            {
-                WorkerReportsProgress = true
-            };
+            worker = new BackgroundWorker { WorkerReportsProgress = true };
             worker.DoWork += Worker_DoWork;
             worker.ProgressChanged += Worker_ProgressChanged;
             worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
@@ -84,41 +81,60 @@ namespace FileEncryptorApp
         {
             string key = string.Empty;
             keyTextBox.Dispatcher.Invoke(() => key = keyTextBox.Text);
-            return key.PadRight(32); 
+            return key.PadRight(32);
+        }
+
+        private (Aes, ICryptoTransform) PrepareEncryption()
+        {
+            Aes aes = Aes.Create();
+            byte[] keyBytes = Encoding.UTF8.GetBytes(GetEncryptionKey());
+            aes.Key = keyBytes;
+            aes.IV = keyBytes.Take(16).ToArray();
+
+            ICryptoTransform cryptoTransform = isEncryptionMode ? aes.CreateEncryptor() : aes.CreateDecryptor();
+            return (aes, cryptoTransform);
+        }
+
+        private void ProcessFile(string tempFilePath, ICryptoTransform cryptoTransform)
+        {
+            using (FileStream inputStream = new FileStream(selectedFilePath, FileMode.Open, FileAccess.Read))
+            using (FileStream outputStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
+            using (CryptoStream cryptoStream = new CryptoStream(outputStream, cryptoTransform, CryptoStreamMode.Write))
+            {
+                byte[] buffer = new byte[1024];
+                long totalBytes = inputStream.Length, totalRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cryptoStream.Write(buffer, 0, bytesRead);
+                    totalRead += bytesRead;
+                    worker.ReportProgress((int)((double)totalRead / totalBytes * 100));
+                }
+            }
+        }
+
+        private void ReplaceOriginalFile(string tempFilePath)
+        {
+            File.Copy(tempFilePath, selectedFilePath, overwrite: true);
+            File.Delete(tempFilePath);
         }
 
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            string key = GetEncryptionKey();
             string tempFilePath = Path.GetTempFileName();
+            var (aes, cryptoTransform) = PrepareEncryption();
 
-            using (FileStream inputStream = new FileStream(selectedFilePath, FileMode.Open, FileAccess.Read))
-            using (FileStream outputStream = new FileStream(tempFilePath, FileMode.Create, FileAccess.Write))
-            using (Aes aes = Aes.Create())
+            try
             {
-                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-                aes.Key = keyBytes;
-                aes.IV = keyBytes.Take(16).ToArray();
-
-                ICryptoTransform cryptoTransform = isEncryptionMode ? aes.CreateEncryptor() : aes.CreateDecryptor();
-                using (CryptoStream cryptoStream = new CryptoStream(outputStream, cryptoTransform, CryptoStreamMode.Write))
-                {
-                    byte[] buffer = new byte[1024];
-                    long totalBytes = inputStream.Length, totalRead = 0;
-                    int bytesRead;
-
-                    while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        cryptoStream.Write(buffer, 0, bytesRead);
-                        totalRead += bytesRead;
-                        worker.ReportProgress((int)((double)totalRead / totalBytes * 100));
-                    }
-                }
+                ProcessFile(tempFilePath, cryptoTransform);
+                ReplaceOriginalFile(tempFilePath);
+                e.Result = selectedFilePath;
             }
-
-            File.Copy(tempFilePath, selectedFilePath, overwrite: true);
-            File.Delete(tempFilePath);
-            e.Result = selectedFilePath;
+            finally
+            {
+                aes.Dispose();
+            }
         }
 
         private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
